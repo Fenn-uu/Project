90cb661b523df770941b62f6a5d217a4514fe10d
Fernand Denoel <fernand.denoel@angstrom.uu.se>
2021-03-02 13:22:44 +0100

    Added missing files and profiling exercices

---
+++ Day 2/Animals/birds.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 18 17:03:14 2021
+
+@author: ferde233
+"""
+
+class Birds:
+    def __init__(self):
+        ''' Constructor for this class. '''
+        # Create some member animals
+        self.members = ['Sparrow', 'Robin', 'Duck']
+        self.d = []
+        self.h = ['Sparrow', 'Robin', 'Duck']
+
+
+    def printMembers(self):
+         print('Printing members of the Birds class')
+         for member in self.members:
+             print('\t%s ' % member)
+             
+    def dangerous(self): 
+        def printMembers(self) :
+            print('Printing dangerous members of the Birds class')
+            for member in self.d :
+                print('\t%s' % member)
+    
+    def harmless(self):
+        def printMembers(self) :
+            print('Printing harmless members of the Birds class')
+            for member in self.h :
+                print('\t%s' % member)
